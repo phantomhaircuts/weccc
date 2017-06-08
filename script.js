6ca6af5a2fed00e90d3d3918d4901d286df633e4
Erik Anderson <itserikanderson@gmail.com>
2017-06-08 10:28:16 -0400

    beautified

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-    //Remove Offset when screen is below 1146px
-    $(window).resize(function() {
-        if ($(window).width() < 1146) {
-            $('.content-left').removeClass('offset-lg-2');
-        } else {
-            $('.content-left').addClass('offset-lg-2');
-        }
-    }).resize();
-    //Prevent submit for mailing list form
-    $('form').submit(function(event) {
-        event.preventDefault();
-        $('.form-alert').text('Thanks for signing up!');
-        $('input[type=text], textarea').val('');
-    });
-    //Toggle Slider Function
-    $('.about-toggle').on('click', function() {
-        $('.slider').toggleClass('close');
-        $('.main-content-container').toggleClass('hidden');
-    });
+  //Remove Offset when screen is below 1146px
+  $(window).resize(function() {
+    if ($(window).width() < 1146) {
+      $('.content-left').removeClass('offset-lg-2');
+    } else {
+      $('.content-left').addClass('offset-lg-2');
+    }
+  }).resize();
+  //Prevent submit for mailing list form
+  $('form').submit(function(event) {
+    event.preventDefault();
+    $('.form-alert').text('Thanks for signing up!');
+    $('input[type=text], textarea').val('');
+  });
+  //Toggle Slider Function
+  $('.about-toggle').on('click', function() {
+    $('.slider').toggleClass('close');
+    $('.main-content-container').toggleClass('hidden');
+  });
 });
